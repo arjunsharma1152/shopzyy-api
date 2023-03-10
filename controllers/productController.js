3652cf43cd65d81e8c372f2616ef228c19bf86bb
Arjun Sharma <77678077+arjunsharma1152@users.noreply.github.com>
2023-03-10 13:36:11 +0000

    spelling mistake corrected

--- controllers/productController.js
+++ controllers/productController.js
@@ -28,9 +28,9 @@ exports.getAllProduct = async (req,res) => {
 
     // const tours = await Product.find().where('duration').equals(5);
 
-    res.header('Acsess-Control-Allow-Origin', '*');
-    res.header('Acsess-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Acsess-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     res.status(200).json({
         status: 'success',
