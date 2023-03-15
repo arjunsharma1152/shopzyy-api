5b41dafa5df9841f43e64cc627a22c9e66014259
Arjun <arjunsk923@gmail.com>
2023-03-15 17:37:30 +0530

    headers are added in GET request

--- controllers/directoryController.js
+++ controllers/directoryController.js
@@ -2,8 +2,13 @@ const Directory = require('../models/directoryModel');
 
 exports.getDirectory = async (req,res) => {
     const directory = await Directory.find();
-
+    
     try{
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
     res.status(200).json({
         status: 'success',
         results: directory.length,
